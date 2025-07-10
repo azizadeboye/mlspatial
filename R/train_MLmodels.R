cec63c267cc97be96c71be5d11f7cab32cd0567d
Adeboye Azeez <54674659+azizadeboye@users.noreply.github.com>
2025-07-10 06:12:15 +0200

    Update train_MLmodels.R

--- R/train_MLmodels.R
+++ R/train_MLmodels.R
@@ -1,4 +1,11 @@
 #' Train Random Forest
+#' Train a Random Forest model
+#'
+#' @param data A data frame containing the training data.
+#' @param formula A formula describing the model structure.
+#' @param ntree Number of trees to grow.
+#' @param seed Random seed for reproducibility.
+
 #' @export
 train_rf <- function(data, formula, ntree = 500, seed = 123) {
   set.seed(seed)
@@ -6,6 +13,17 @@ train_rf <- function(data, formula, ntree = 500, seed = 123) {
 }
 
 #' Train XGBoost
+#' Train an XGBoost model
+#'
+#' @param data A data frame with the training data.
+#' @param formula A formula defining the model structure.
+#' @param nrounds Number of boosting iterations.
+#' @param max_depth Maximum tree depth.
+#' @param eta Learning rate.
+#' @importFrom methods setClass
+#' @importFrom xgboost xgb.DMatrix
+#' @importFrom xgboost xgboost
+
 #' @export
 train_xgb <- function(data, formula, nrounds = 100, max_depth = 4, eta = 0.1) {
   model.matrix(formula, data = data)[,-1] -> x
@@ -16,6 +34,11 @@ train_xgb <- function(data, formula, nrounds = 100, max_depth = 4, eta = 0.1) {
 }
 
 #' Train SVR
+#' Train a Support Vector Regression model
+#'
+#' @param data A data frame containing the training data.
+#' @param formula A formula specifying the model.
+
 #' @export
 train_svr <- function(data, formula) {
   e1071::svm(formula, data = data, type = "eps-regression", kernel = "radial")
